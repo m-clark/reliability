caf2ae5211a76c98723475696198d78b3895e79a
Michael Clark <micl@umich.edu>
2018-10-15 08:43:25 -0400

    minor edits, updates

--- code/Najafabadi.R
+++ code/Najafabadi.R
@@ -9,9 +9,9 @@ Alpha <- function(all_data) {
   B = c()
   B2 = c()
   
-  if (p <= 2)
+  if (p <= 2) {
     print("There is no solution for dimension less than 3")
-  else {
+  } else {
     # Cronbach’s alpha calculation
     Alpha_Cronbach <- function(data) {
       k = dim(data)[2]
@@ -20,6 +20,7 @@ Alpha <- function(all_data) {
                                sum(cov(data, use = "na.or.complete")))
       return(alpha)
     }
+    
     # Ordinal’s theta calculation
     theta_ordinal = function(data) {
       k = dim(data)[2]
@@ -27,6 +28,7 @@ Alpha <- function(all_data) {
       theta = k / (k - 1) * (1 - 1 / max(eigen(cor(data, use = "na.or.complete"))$value))
       return(theta)
     }
+    
     # Bayes estimator
     Bayes <- function(data) {
       n = dim(data)[1]
@@ -34,20 +36,24 @@ Alpha <- function(all_data) {
       
       r = k / (k - 1) * (1 - tr(mixed.cor(data)$rho) / sum(mixed.cor(data)$rho))   # removed deprecated polycor=T
       theta = theta_ordinal (data)
-      a = (1 - theta) ^ 2 / 100
+      a = (1 - theta)^2 / 100
       b = (1 - theta) / 100
       
       f1 = function(x) {
-        x * (1 - x) ^ (min(30, a + (n - 3) / 2)) * exp(-b * (1 - x)) * (1 + (1 -
-                                                                               x) / (k - r * (k - 1))) ^ (-(n - 1) * k / 2)
+        x * (1 - x)^(min(30, a + (n - 3) / 2)) *
+          exp(-b * (1 - x)) * 
+          (1 + (1 - x) / (k - r * (k - 1)))^(-(n - 1) * k / 2)
       }
+      
       f2 = function(x) {
-        (1 - x) ^ (min(30, a + (n - 3) / 2)) * exp(-b * (1 - x)) * (1 + (1 - x) /
-                                                                      (k - r * (k - 1))) ^ (-(n - 1) * k / 2)
+        (1 - x)^(min(30, a + (n - 3) / 2)) * 
+          exp(-b * (1 - x)) * 
+          (1 + (1 - x) / (k - r * (k - 1)))^(-(n - 1) * k / 2)
       }
-      bayes = integrate(f1, lower = -Inf, upper = 1)$value / (0.e-20 + integrate(f2, lower =
-                                                                                   -Inf,
-                                                                                 upper = 1)$value)
+      
+      bayes = integrate(f1, lower = -Inf, upper = 1)$value / 
+        (0.e-20 + integrate(f2, lower = -Inf, upper = 1)$value)
+      
       return(bayes)
     }
     
@@ -57,25 +63,25 @@ Alpha <- function(all_data) {
       
       r = k / (k - 1) * (1 - tr(mixed.cor(data)$rho) / sum(mixed.cor(data)$rho))   # removed deprecated polycor=T
       theta = theta_ordinal (data)
-      a = (1 - theta) ^ 2 / 100
+      a = (1 - theta)^2 / 100
       b = (1 - theta) / 100
       c = -10
       
       f1 = function(x) {
-        exp(-c * x) * (1 - x) ^ (min(30, a + (n - 3) / 2)) * exp(-b * (1 - x)) *
-          (1 + (1 - x) / (k - r * (k - 1))) ^ (-(n - 1)
-                                               *
-                                                 k / 2)
+        exp(-c * x) * (1 - x)^(min(30, a + (n - 3) / 2)) * 
+          exp(-b * (1 - x)) *
+          (1 + (1 - x) / (k - r * (k - 1)))^(-(n - 1) * k / 2)
       }
+      
       f2 = function(x) {
-        (1 - x) ^ (min(30, a + (n - 3) / 2)) * exp(-b * (1 - x)) * (1 + (1 - x) /
-                                                                      (k - r * (k - 1))) ^ (-(n - 1) * k / 2)
+        (1 - x)^(min(30, a + (n - 3) / 2)) * 
+          exp(-b * (1 - x)) * 
+          (1 + (1 - x) / (k - r * (k - 1))) ^ (-(n - 1) * k / 2)
       }
-      bayes = -log(integrate(f1, lower = -Inf, upper = 1)$value / (0.e-20 +
-                                                                     integrate(
-                                                                       f2, lower = -Inf,
-                                                                       upper = 1
-                                                                     )$value)) / c
+      
+      bayes = -log(integrate(f1, lower = -Inf, upper = 1)$value / 
+                     (0.e-20 + integrate(f2, lower = -Inf, upper = 1)$value)) / c
+      
       return(bayes)
     }
     
@@ -87,34 +93,27 @@ Alpha <- function(all_data) {
     }
     
     list(
-      "Alpha if a Question Deleted" = data.frame(
+      "Alpha if Item Deleted" = data.frame(
         "." = D,
-        row.names = paste("Alpha Without Question.", 1:p, "=")
+        row.names = paste("Alpha Without Item", 1:p, "=")
       ) ,
-      "Cronbach’s
-Alpha for all Question=" = Alpha_Cronbach(all_data),
-      "Ordinal Theta
-if a Question Deleted" = data.frame(
-  "." = Q,
-  row.names = paste("Theta Without Question.", 1:p, "=")
-) ,
-"Ordinal Theta for all
-Question=" = theta_ordinal(all_data),
-"Bayesian-Squared-Error Alpha if a Question
-Deleted" = data.frame(
-  "." = B,
-  row.names = paste("Bayesian-Squared-Error Alpha Without Question.", 1:p, "=")
-) ,
-" Bayesian-Squared-
-Error Alpha for
-all Question=" = Bayes(all_data),
-"Bayesian-LINEX Alpha if a Question Deleted" = data.frame(
-  "." = B2,
-  row.names =
-    paste("Bayesian-LINEX Alpha Without Question.", 1:p, "=")
-) ,
-" Bayesian-LINEX Alpha
-for all Question=" = Bayes_LINEX (all_data)
+      "Cronbach’s Alpha for all Items =" = Alpha_Cronbach(all_data),
+      "Ordinal Theta if a Item Deleted" = data.frame(
+        "." = Q,
+        row.names = paste("Theta Without Item", 1:p, "=")
+        ) ,
+      "Ordinal Theta for all Item=" = theta_ordinal(all_data),
+      "Bayesian-Squared-Error Alpha if Item Deleted" = data.frame(
+        "." = B,
+        row.names = paste("Bayesian-Squared-Error Alpha Without Item", 1:p, "=")
+      ) , 
+      "Bayesian-Squared-Error Alpha for all Item=" = Bayes(all_data), 
+      "Bayesian-LINEX Alpha if a Item Deleted" = data.frame(
+        "." = B2,
+        row.names =
+          paste("Bayesian-LINEX Alpha Without Item", 1:p, "=")
+      ) , 
+      "Bayesian-LINEX Alpha for all Item=" = Bayes_LINEX (all_data)
     )
   }
 }
