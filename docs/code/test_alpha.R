24fb8ec0e607581c9b47080f995b8d7c9106c53a
Michael Clark <micl@umich.edu>
2019-01-13 12:31:07 -0500

    add guttman cronbach refs

--- docs/code/test_alpha.R
+++ docs/code/test_alpha.R
@@ -20,8 +20,52 @@ alpha_bayes = stan(file = 'code/alpha.stan',
 print(alpha_bayes, digits=3, pars = c('alpha', 'alpha2'))
 launch_shinystan(alpha_bayes)
 
+library(tidybayes)
+alpha_draws = spread_draws(alpha_bayes, alpha2)
+
+head(alpha_draws)
+
+mean_qi(alpha_draws)
+
+library(gganimate)
+
+n_hops = 100
+
+alpha_animate = alpha_draws %>% 
+  ggplot(aes(x = alpha2, y= 'alpha', group=.draw)) +
+  # ggplot(aes(x = alpha2, y= 'alpha')) +
+  geom_point(x = .813, size=10, color='#ff5500', alpha=.25) +
+  geom_point(alpha=.05, size=3, position = position_jitter()) +
+  # transition_states(.draw, transition_length = 1, state_length = 1) +
+  transition_reveal(.draw) + # group .draw to keep dots persistent
+  theme_trueMinimal()
+    
+
+# animate(alpha_animate, nframes = n_hops*2)
+animate(alpha_animate, nframes = n_hops*1)
+anim_save('img/alpha_unc.gif')
+
+
+
+alpha_animate = alpha_draws %>% 
+  mutate(alpha = 'alpha') %>% 
+  ggplot(aes(y = alpha2, x= alpha, group=.draw)) +
+  # ggplot(aes(x = alpha2, y= 'alpha')) +
+  geom_point(y = .813, size=10, color='#ff5500', alpha=.25) +
+  geom_sina(aes(), bins=100) +
+  # transition_states(.draw, transition_length = 1, state_length = 1) +
+  transition_reveal(.draw) + # group .draw to keep dots persistent
+  theme_trueMinimal()
+
+
+# animate(alpha_animate, nframes = n_hops*2)
+animate(alpha_animate, nframes = n_hops*1)
+anim_save('img/alpha_unc.gif')
+
+
+
 alpha(scale(neuroticism_no_na), digits = 2)
-MBESS::ci.reliability(scale(neuroticism_no_na), type='alpha' interval.type = 'ml')
+MBESS::ci.reliability(scale(neuroticism_no_na), type='alpha', interval.type = 'ml')
 
 # for ave
 library(lavaan)
